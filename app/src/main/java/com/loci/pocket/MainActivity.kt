fbcc83cb59aa714eb41f90b47b66c3d72ea23552
kdbswo <kdbswo@gmail.com>
2023-10-22 04:44:49 +0900

    Add Card

--- app/src/main/java/com/loci/pocket/MainActivity.kt
+++ app/src/main/java/com/loci/pocket/MainActivity.kt
@@ -6,11 +6,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -19,11 +24,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.loci.pocket.ui.theme.PocketTheme
 
 class MainActivity : ComponentActivity() {
@@ -62,8 +71,89 @@ fun PocketCard() {
             .graphicsLayer {
                 rotationY = animationDegree
             }
-            .background(Color.Gray)
+//            .background(Color.Gray)
     ) {
+        if (animationDegree > 90f) {
+            PocketFront()
+        } else {
+            PocketBack()
+        }
 
     }
 }
+
+@Composable
+fun PocketFront() {
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xffffd700), shape = RoundedCornerShape(30.dp))
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(10.dp)
+                .background(Color.Black, shape = RoundedCornerShape(30.dp)),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(painter = painterResource(id = R.drawable.card), contentDescription = null)
+        }
+    }
+}
+
+
+@Composable
+fun PocketBack() {
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xffffd700), shape = RoundedCornerShape(30.dp))
+//            .graphicsLayer {
+//                rotationY = 180f
+//            }
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(10.dp)
+                .background(Color.Red, shape = RoundedCornerShape(30.dp)),
+            contentAlignment = Alignment.Center
+
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(painter = painterResource(id = R.drawable.img), contentDescription = null)
+                Spacer(modifier = Modifier.padding(20.dp))
+                Text(
+                    text = "파이리",
+                    fontSize = 30.sp,
+                    color = Color.White,
+                    fontWeight = FontWeight.ExtraBold
+                )
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
